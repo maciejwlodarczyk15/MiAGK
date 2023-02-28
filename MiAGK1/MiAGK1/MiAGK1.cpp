d8bc14cbf3625b3d9b8141a8152f52a876b95f92
Maciej Włodarczyk <maciejwlodarczyk15@gmail.com>
2023-02-28 16:45:03 +0100

    Hello world

---
+++ MiAGK1/MiAGK1/MiAGK1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    // Plik generujacy plik w formacie
+    // biblioteka do zapisu obrazu
+    // ustawienie rozdzielczosci i koloru wypelnionego okna
+    
+}
