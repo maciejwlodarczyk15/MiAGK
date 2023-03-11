262619b311ddce6a4718c261914a8e52dbbdf45a
Maciej Włodarczyk <maciejwlodarczyk15@gmail.com>
2023-03-11 17:39:32 +0100

    Added float3 * float4x4 multiplication

--- MiAGK1/MiAGK1/MiAGK1.cpp
+++ MiAGK1/MiAGK1/MiAGK1.cpp
@@ -21,5 +21,17 @@ int main()
     buffer.Triangle({-0.5f, 0.9f, 0.0f }, { 0.8f, 0.0f, -1.0f }, { 0.3f, 0.0f, 1.0f },
                     { 1.0f, 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f }, depthBuffer);
 
+    float4x4 testMatrix(1.0f,  2.0f,  3.0f,  4.0f,
+                        5.0f,  6.0f,  7.0f,  8.0f,
+                        9.0f,  10.0f, 11.0f, 12.0f,
+                        13.0f, 14.0f, 15.0f, 16.0f);
+    testMatrix.WriteToConsole(); // Correct
+
+    float3 smallFloat({ 1.0f, 2.0f, 3.0f });
+
+    float3 result = smallFloat * testMatrix;
+    result.WriteToConsole(); // Correct
+
+
     buffer.Save();
 }
--- MiAGK1/MiAGK1/NTypes.h
+++ MiAGK1/MiAGK1/NTypes.h
@@ -1,4 +1,41 @@
 #pragma once
+#include <iostream>
+
+struct float4x4
+{
+	float m[4][4];
+
+	float4x4()
+	{
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				m[i][j] = 0.0f;
+			}
+		}
+	}
+
+	float4x4(float m00, float m01, float m02, float m03,
+		float m10, float m11, float m12, float m13,
+		float m20, float m21, float m22, float m23,
+		float m30, float m31, float m32, float m33)
+	{
+		m[0][0] = m00; m[0][1] = m01; m[0][2] = m02; m[0][3] = m03;
+		m[1][0] = m10; m[1][1] = m11; m[1][2] = m12; m[1][3] = m13;
+		m[2][0] = m20; m[2][1] = m21; m[2][2] = m22; m[2][3] = m23;
+		m[3][0] = m30; m[3][1] = m31; m[3][2] = m32; m[3][3] = m33;
+	}
+
+	void WriteToConsole()
+	{
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				std::cout << m[i][j] << ", ";
+			}
+			std::cout << std::endl;
+		}
+	}
+};
+
 struct float3 
 {
 	float x, y, z;
@@ -19,12 +56,28 @@ struct float3
 		xd.z = z * o;
 		return xd;
 	}
-};
+	float3 operator*(float4x4 m)
+	{
+		float3 result;
+		result.x = this->x * m.m[0][0] + this->y * m.m[1][0] + this->z * m.m[2][0] + 1.0f * m.m[3][0];
+		result.y = this->x * m.m[0][1] + this->y * m.m[1][1] + this->z * m.m[2][1] + 1.0f * m.m[3][1];
+		result.z = this->x * m.m[0][2] + this->y * m.m[1][2] + this->z * m.m[2][2] + 1.0f * m.m[3][2];
+		float w  = this->x * m.m[0][3] + this->y * m.m[1][3] + this->z * m.m[2][3] + 1.0f * m.m[3][3];
 
+		// result / w = perspective
+		if (w != 0)
+		{
+			result.x /= w;
+			result.y /= w;
+			result.z /= w;
+		}
 
-struct float2 
-{
-	float x, y;
+		return result;
+	}
+	void WriteToConsole()
+	{
+		std::cout << "(" << x << ", " << y << ", " << z << ")\n";
+	}
 };
 
 struct float4
